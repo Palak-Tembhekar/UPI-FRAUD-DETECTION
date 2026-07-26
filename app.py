d33a442b76111f3b2f6a8396079f84c2466f4c5a
Palak-Tembhekar <palaktembhekar@gmail.com>
2026-07-26 08:32:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ if st.button("🔍 analyze risk level",use_container_width=True):
        st.progress(1.0)
     else:
        input_data = pd.Dataframe([[amount,hour,drain_ratio,speed_kmh]],
-                                 columns=['amount'.,'hour','drain_ratio','speed_kmh'])
+                                 columns=['amount','hour','drain_ratio','speed_kmh'])
        prob_fraud = model.predict_proba(input_data)[0][1]
        fraud_pct = prob_fraud * 100
        st.subheader("🎯 Risk Assessment Result")
